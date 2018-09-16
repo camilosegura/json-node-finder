6477acf22baa0157c37928d6d51a02b1b4d7c2c7
Alejandro San Martin <camilosegurar@gmail.com>
2018-09-16 11:00:10 -0400

    Added graphQL Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
-const Index = () => <div>Hello React!</div>;
+const client = new ApolloClient();
+
+const Index = () => (
+  <ApolloProvider client={client}>
+    <div>Hello React!</div>
+  </ApolloProvider>
+);
 
 ReactDOM.render(<Index />, document.getElementById('index'));
